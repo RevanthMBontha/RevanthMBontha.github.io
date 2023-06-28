854261c7b376239ac5c2264f085d3d476d5f8bf1
Revanth M. Bontha <69347893+RevanthMBontha@users.noreply.github.com>
2023-06-28 17:14:39 +0530

    Updates to Portfolio section

---
+++ src/components/portfolio/Data.jsx
@@ -0,0 +1,42 @@
+import comfysloth from '../../assets/comfysloth.png';
+import theCocktailDatabase from '../../assets/theCocktailDatabase.png';
+import natureWalks from '../../assets/natureWalks.png';
+import personalWebsite from '../../assets/personalWebsite.png';
+
+export const projectsData = [
+  {
+    id: 1,
+    image: comfysloth,
+    title: 'ComfySloth',
+    tagline: 'Mock E-commerce site with payments',
+    link: 'https://comfy-sloth-revanthmbontha.netlify.app/',
+  },
+  {
+    id: 2,
+    image: theCocktailDatabase,
+    title: 'The Cocktail Database',
+    tagline: 'A library of more than 80 Cocktails',
+    link: 'https://the-cocktail-database-revanthmbontha.netlify.app/',
+  },
+  {
+    id: 3,
+    image: personalWebsite,
+    title: 'Personal Website',
+    tagline: 'Probably the one you are looking at right now!',
+    link: 'https://comfy-sloth-revanthmbontha.netlify.app/',
+  },
+  {
+    id: 4,
+    image: 'Work1',
+    title: 'React Components Gallery',
+    tagline: 'A set of reusable React Components built from scratch',
+    link: 'https://lol.com',
+  },
+  {
+    id: 5,
+    image: natureWalks,
+    title: 'NatureWalks',
+    tagline: 'Mock eco-tourism site',
+    link: 'https://nature-walks-revanthmbontha.netlify.app/',
+  },
+];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './portfolio.css';
+import Works from './Works';
 
 const Portfolio = () => {
   return (
     <section className="portfolio section" id="portfolio">
       <h2 className="section__title">Portfolio</h2>
       <span className="section__subtitle">My Work!</span>
+
+      <Works />
     </section>
   );
 };
